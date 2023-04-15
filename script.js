6465436ecf52e7d253776384b337a9cbb219877a
Jose Mosquera <jojumofu@gmail.com>
2023-04-15 14:11:21 -0500

    chore: reform the structure of html, css and js files

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
-function encrypt() {
-    var inputText = document.getElementById("inputText").value;
-    var outputText = "";
+function procesar() {
+    var entrada = document.getElementById("entrada").value;
+    var accion = document.getElementById("accion").value;
+    var resultado = document.getElementById("resultado");
 
-    for (var i = 0; i < inputText.length; i++) {
-        var charCode = inputText.charCodeAt(i);
-        var encryptedCharCode = charCode + 10; // Shift by 10 character
-        outputText += String.fromCharCode(encryptedCharCode);
+    if (accion === "encriptar") {
+        resultado.value = encriptar(entrada);
+    } else if (accion === "desencriptar") {
+        resultado.value = desencriptar(entrada);
     }
-
-    document.getElementById("outputText").value = outputText;
 }
+
+function encriptar(texto) {
+    // Reemplazar letras según la regla de encriptación
+    texto = texto.replace(/e/g, "enter");
+    texto = texto.replace(/i/g, "imes");
+    texto = texto.replace(/a/g, "ai");
+    texto = texto.replace(/o/g, "ober");
+    texto = texto.replace(/u/g, "ufat");
+
+    return texto;
+}
+
+function desencriptar(texto) {
+    // Reemplazar letras según la regla de desencriptación
+    texto = texto.replace(/enter/g, "e");
+    texto = texto.replace(/imes/g, "i");
+    texto = texto.replace(/ai/g, "a");
+    texto = texto.replace(/ober/g, "o");
+    texto = texto.replace(/ufat/g, "u");
+
+    return texto;
+}
+
+function limitarMinusculas(input) {
+    var inputValue = input.value;
+    var regex = /[^a-z\s!]/g;
+    var newValue = inputValue.replace(regex, '').toLowerCase();
+    input.value = newValue;
+}
+
+
+function copiarTexto() {
+    /* Obtener el elemento de entrada de texto encriptado */
+    var resultadoInput = document.getElementById("resultado");
+
+    /* Seleccionar el texto en el input */
+    resultadoInput.select();
+    resultadoInput.setSelectionRange(0, 99999); /* Para dispositivos móviles */
+
+    /* Copiar el texto al portapapeles */
+    document.execCommand("copy");
+
+    /* Mostrar un mensaje de éxito */
+    alert("Texto resultado copiado al portapapeles: " + resultadoInput.value);
+}
+
